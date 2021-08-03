81fb03697bc9ff69eb7ff46d6e3a9e16339fa6bd
Mike Jadick <mike@jadick.dev>
2021-08-03 14:07:39 -0600

    Add postcss to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,8 @@ const htmlConfig = {
 
 const cssConfig = {
   test: /\.css$/i,
-  use: ['style-loader', 'css-loader'],
+  exclude: /node_modules/,
+  use: ['style-loader', 'css-loader', 'postcss-loader'],
 }
 
 const minifyConfig = {
